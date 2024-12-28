39311d31949e79c3ed7e815d8d003e5829006a20
sergiorios777 <sergio.rios777@gmail.com>
2024-12-28 10:33:06 -0500

    inicio de configuración de vistas básicas

---
+++ app/Views/app/panel/panel.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div class="container">
+        <div>
+            <h1>Panel administrativo</h1>
+        </div>
+        <div>
+            <a href="/logout">Logout</a>
+            <p>--------</p>
+            <ul>
+                <li>Usuarios:</li>
+                    <ul>
+                        <li>Solicitudes de creación</li>
+                        <li>Lista de usuarios</li>
+                        <li>Editar</li>
+                    </ul>
+                <li>Requerimientos</li>
+                <li>Configuración</li>
+            </ul>
+        </div>
+        <div>
+            <p>Resultados</p>
+        </div>
+    </div>
+</body>
+</html>
